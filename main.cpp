11bb529193bbac57e261d09f25550e93929503f3
nhershy <nhershy@gmail.com>
2017-02-06 14:55:20 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int vecSize = 0;
+    int input;
+    vector<int> vec;
+    
+    cout << "How big is your vector: ";
+    cin >> vecSize;
+    cout << "Enter your data in the vector: ";
+    for (int i=0;i<vecSize;i++)
+    {
+        cin >> input;
+        vec.push_back(input);
+    }
+
+    //print vec with iterator
+    vector<int>::iterator it;
+    for (it=vec.begin(); it!=vec.end(); ++it)
+        cout << *it << " ";
+
+    return 0;
+}
